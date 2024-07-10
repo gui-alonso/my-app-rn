827cd7ff072d68bbc189b938e04f3f3d11b222cd
Guilherme <=>
2024-07-10 13:26:38 -0300

    add arquivo Body e atualização file App.tsx

--- App.tsx
+++ App.tsx
@@ -2,12 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Header  from './componentes/Header.js';
+import Body  from './componentes/Body.js';
 
 export default class App extends React.Component {
   render(){
     return(
       <View style={{marginTop:20}}>
         <Header></Header>
+        <Body></Body>
       </View>
     )
   }
---
+++ componentes/Body.js
@@ -0,0 +1,11 @@
+// criando compoente body nessa mesma branch do header!!
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const Body = () => {
+    return (
+        <Text>Olá do meu arquivo body!</Text>
+    )
+}
+
+export default Body;
